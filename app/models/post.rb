1b630b847fb87dd787f09949f259f9febef89fef
The Topic Man <tomasz.kowalewski@codesthq.com>
2019-03-18 11:22:49 +0100

    brakeman

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :body
+
+  def self.evaluate_user_input
+    eval(params)
+  end
+
+  def evaluate_user_input
+    self.class.evaluate_user_input
+  end
 end
